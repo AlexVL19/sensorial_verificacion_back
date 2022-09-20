047a9bfee31df2c04ed9e8bd634b1d83f0920080
AlexVL19 <avalencia190@misena.edu.co>
2022-09-20 09:55:11 -0500

    Documentación

--- app/Http/Controllers/FormularioController.php
+++ app/Http/Controllers/FormularioController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\DB;
 class FormularioController extends Controller
 {
     public function traerItems() {
+
+        //Trae los campos necesarios de cada ítem y trae de vuelta el resultado de dicha query
         $sql = "SELECT id_item, item, categoria_item FROM sensorial_verificacion_items";
 
         $result = DB::connection()->select(DB::raw($sql));
@@ -17,6 +19,7 @@ class FormularioController extends Controller
 
     public function traerMetodosFormulario1() {
 
+        //Trae todos los métodos presentes en la tabla, y trae de vuelta el resultado de la query
         $sql_metodos = "SELECT * FROM sensorial_verificacion_metodos";
 
         $result = DB::connection()->select(DB::raw($sql_metodos));
@@ -25,6 +28,8 @@ class FormularioController extends Controller
     }
 
     public function traerCategorias () {
+
+        // Trae el ID de la categoría de la tabla de categorías, y trae de vuelta el resultado de la query
         $sql_categorias = "SELECT id_categoria_item FROM sensorial_verificacion_categorias_items";
 
         $result = DB::connection()->select(DB::raw($sql_categorias));
@@ -34,7 +39,10 @@ class FormularioController extends Controller
 
     public function returnRequest(Request $request) {
 
+        //Recorre cada iteración de lo que está recibiendo
         foreach($request->formulario as $formularios) {
+
+            //Recorre lo que está dentro de cada respuesta y empieza a insertar
             foreach($formularios as $formulario) {
                 $sql_insert = "INSERT INTO sensorial_verificacion_bitacora (id_item, id_metodo, revisado)
                 VALUES (?, ?, ?)";
